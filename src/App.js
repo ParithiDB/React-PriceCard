8dd8aaec9ceda5e2226871e2a519736d57336b11
ParithiDB <samu.aidindia@gmail.com>
2023-06-23 23:44:32 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Square from './Components/Square/Square';
+import PriceCard from './Components/PriceCard/PriceCard';
+
+function App() {
+  return (
+    <div className="App">
+     <PriceCard />
+    </div>
+  );
+}
+
+export default App;
